53ce09557d6894697f95de07d5fc65401bc748a4
Vladimir Perepelica <vladimir.perepelica@onix-systems.com>
2023-05-22 14:48:44 +0300

    REACT-274 #7 cleanup preview decorator

--- .storybook/decorators/Preview.tsx
+++ .storybook/decorators/Preview.tsx
@@ -2,8 +2,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from '../../src/store/store';
-import App from '../../src/App';
-import React from 'react';
 
 function Preview(Story): JSX.Element {
   return (
