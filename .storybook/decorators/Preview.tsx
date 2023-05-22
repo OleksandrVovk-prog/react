c460982223e16e32d787976e25c0e3d6db75c499
Vladimir Perepelica <vladimir.perepelica@onix-systems.com>
2023-05-22 15:27:37 +0300

    REACT-274 #7 mock files for storybook

--- .storybook/decorators/Preview.tsx
+++ .storybook/decorators/Preview.tsx
@@ -2,16 +2,13 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from '../../src/store/store';
+import authMock from '../mocks/auth';
 
 function Preview(Story): JSX.Element {
   const mainState = store.getState();
   store.replaceReducer((state = mainState) => ({
     ...state,
-    auth: {
-      ...state.auth,
-      id: 1,
-      token: 'token',
-    }
+    auth: { ...state.auth, ...authMock }
   }));
   return (
     <BrowserRouter>
