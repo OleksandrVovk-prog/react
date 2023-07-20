6e0539145b8b2e2ed41e8718b66c4c7b7e3385c5
Vladimir Perepelica <vladimir.perepelica@onix-systems.com>
2023-07-20 16:48:26 +0300

    REACT-274 #6 sonar fixes

--- src/utils/tests/validators.test.ts
+++ src/utils/tests/validators.test.ts
@@ -1,13 +1,13 @@
 import { loginSchema, editProfileSchema } from '../validators';
+import user, { userCredentials } from '../../mocks/user';
 
 describe('validators', () => {
   test('loginSchema is valid', async () => {
-    const validObject = { password: '123', username: 'John' };
-    const isValid = await loginSchema.isValid(validObject);
+    const isValid = await loginSchema.isValid(userCredentials);
     expect(isValid).toBe(true);
   });
   test('editProfileSchema  is valid', async () => {
-    const validObject = { firstName: 'John', lastName: 'Doe' };
+    const validObject = { firstName: user.firstName, lastName: user.lastName };
     const isValid = await editProfileSchema.isValid(validObject);
     expect(isValid).toBe(true);
   });
